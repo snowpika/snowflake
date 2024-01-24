9cfc1242b6562b7fb9707648465a2af06f7239f7
snowpika <157275939+snowpika@users.noreply.github.com>
2024-01-24 02:00:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
 import streamlit
 import pandas
-
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('Recipies for Food 🥑 and Yummy Shakes 🥝🍇')
 
 streamlit.header('a basic recipe')
 streamlit.text('eggs')
 streamlit.text('parmesan cheese')
-streamlit.text('soup, yum: 🥣')
-streamlit.text('dont know what this is: 🥗')
-streamlit.text('chicken broth: 🐔')
+
 streamlit.text('avocado!: 🥑')
 streamlit.text('lastly, bread: 🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
