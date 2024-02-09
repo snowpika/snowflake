9b6cd1e8239ac1f598a1cf38a0bed4fa891c19fd
snowpika <157275939+snowpika@users.noreply.github.com>
2024-02-09 01:55:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ streamlit.dataframe(fruits_to_show)
 # streamlit.dataframe(my_fruit_list)
 
 -- ------------------------------------------------------------
+streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
